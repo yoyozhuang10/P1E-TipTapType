d2a74330021def74a539741e4927bf746de72dd4
carlxw <335563110@gapps.yrdsb.ca>
2021-10-29 10:00:29 -0400

    Press k to cycle words

--- MyWorld.java
+++ MyWorld.java
@@ -1,21 +1,81 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.io.*;
+import java.util.*;
+import greenfoot.GreenfootImage;
 
 /**
- * Write a description of class MyWorld here.
+ * World where user begins typing.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Carl Wang
+ * @version 2021.10.29
  */
 public class MyWorld extends World
 {
-
+    ArrayList<String> wordList = new ArrayList<String>();
+    Label text;
+    boolean kDown;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MyWorld() throws InterruptedException
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        extractWords(); // Extracts list of words from words.txt --> ArrayList
+    }
+    
+    public void act()
+    {
+        // To run once after being x key pressed down once
+        if (kDown != Greenfoot.isKeyDown("k"))
+        {
+            kDown = !kDown;
+            if (kDown)
+            {
+                if (numberOfObjects() == 0) // Empty world
+                {
+                    text = new Label(wordList.get(rand()), 75);
+                    addObject(text, getWidth()/2, getHeight()/2);
+                }  
+                else // Remove existing text to replace
+                {
+                    removeObject(text);
+                    text = new Label(wordList.get(rand()), 75);
+                    addObject(text, getWidth()/2, getHeight()/2);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Reads word.txt and stores data into a data structure (ArrayList)
+     * 
+     */
+    public void extractWords()
+    {
+        try
+        {
+            File file = new File("Words.txt"); // Where words are stored
+            Scanner scanner = new Scanner(file); // To extract words
+            
+            while (scanner.hasNextLine())
+            {
+                wordList.add(scanner.nextLine()); // .txt -> ArrayList
+            }
+            
+            scanner.close(); // Destroys scanner
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Something went wrong, MyWorld.java");
+        }
+    }
+    
+    public int rand()
+    {
+        return Greenfoot.getRandomNumber(wordList.size());
     }
 }
---
+++ StartButton.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartButton extends Actor
+{
+    /**
+     * Act - do whatever the StartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (Greenfoot.mouseClicked(this)) 
+        {
+            try
+            {
+                World MyWorld = new MyWorld();
+                Greenfoot.setWorld(MyWorld);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Something went wrong, StartButton.java");
+            }
+        }
+    }
+}
