7cd6dbe1920a1ecf453a186defaac886158f5e81
Yoyo Z <72398571+yoyozhuang10@users.noreply.github.com>
2021-11-01 23:57:05 -0400

    added end screen world

---
+++ EndScreen.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EndScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EndScreen extends World
+{
+
+    /**
+     * Constructor for objects of class EndScreen.
+     * 
+     */
+    public EndScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Label end = new Label("Game Over", 60);
+        addObject(end, getWidth() / 2, getHeight() / 2 - 50);
+        
+        Label score = new Label("Score: " + " 3", 40);
+        addObject(score, getWidth() / 2, getHeight() / 2);
+        
+        Label highScore = new Label("High score: " + "6", 40);
+        addObject(highScore, getWidth() / 2, getHeight() / 2 + 50);
+    }
+}
