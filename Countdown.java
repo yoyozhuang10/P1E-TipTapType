e2c96d8c1d27eb4a99822a15af832582863e79c9
Yoyo Z <72398571+yoyozhuang10@users.noreply.github.com>
2021-11-08 09:12:21 -0500

    added comments

--- Countdown.java
+++ Countdown.java
@@ -4,28 +4,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Plays a short countdown before the game starts
  * 
  * @author Yoyo
- * @version (a version number or a date)
+ * @version 2021.11.08
  */
 public class Countdown extends World
 {
-    double counter = 4.0;
-    double milliCounter = 400.0;
-    Label count;
     /**
      * Constructor for objects of class Countdown.
      * 
      */
+    //Countdown variables
+    double counter = 4.0;
+    double milliCounter = 400.0;
+    Label count;
+    
     public Countdown()
     {    
         // Create a new world with 800x450 cells with a cell size of 1x1 pixels.
         super(800, 450, 1); 
         setBackground(new GreenfootImage("gamebackgroundsmall.png"));
         
+        //Add counter label to screen
         count = new Label((int)counter, 60);
         addObject(count, getWidth() / 2, getHeight() / 2);
     }
     
+    //Counts down from 3 before game starts
     public void act(){
+        //Change counter variable
         milliCounter--;
         if(milliCounter <= counter * 100){
             counter--;
@@ -36,6 +41,7 @@ public class Countdown extends World
             count.setValue("Start!");
         }
         
+        //Switch over to game screen
         if(counter < 0){
             Game g = new Game();
             // Reset Everything
